5146a348b798931dd98f8bf75c8a2989b6377209
huy2401 <huybktphcm@gmail.com>
2024-07-22 09:50:16 +0700

    first commit

---
+++ Timer/src/tuya_device.c
@@ -0,0 +1,28 @@
+#include "gpio_pub.h"
+#include "bk_timer_pub.h" 
+#include "bk_timer.h"
+
+VOID timer_callback()
+{ 
+    static bool FLAG = false;
+    gpio_output(GPIO6, FLAG); // Đảo chân GPIO6
+    FLAG = !FLAG; 
+}
+
+VOID app_init(VOID) 
+{
+    /* WiFi key, len initiation */
+    timer_param_t config = {
+    .channel = BKTIMER3,    // Timer_3     
+    .div = 16,       //        
+    .period = 1000,    // Chu kỳ 1s         
+    .t_Int_Handler = timer_callback // hàm ngắt
+    };
+    // Giá trị ghi vào thanh ghi được tính là value = period * 32 / div = 2000
+    
+    gpio_config(GPIO6, GMODE_OUTPUT);
+    gpio_output(GPIO6, 1); 
+    bk_timer_ctrl(CMD_TIMER_INIT_PARAM, &config); // Cấu hình Timer3
+    bk_timer_ctrl(CMD_TIMER_UNIT_ENABLE, BKTIMER3); // Khởi động Timer3
+    return;
+}
